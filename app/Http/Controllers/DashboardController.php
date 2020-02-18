b64d703c094d050151959f30f4ea31f666fee667
horidhenry <robarnold100@hotmail.com>
2020-02-18 19:31:38 +0000

    b

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -27,7 +27,7 @@ class DashboardController extends Controller
 
     public function upload(Request $request) {
 
-        Log::info(json_encode($request)->file('song'));
+        Log::info(json_encode($request->file('song')));
         // dd($request->file('song'));
 
         $path = $request->file('song')->store('upload');
