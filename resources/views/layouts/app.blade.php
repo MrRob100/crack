d643a2ebdda6b8736b32a59bea8fd4b7ff64384c
horidhenry <robarnold100@hotmail.com>
2020-04-20 08:46:29 +0100

    app cleaned up and has google analytics on it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164034058-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-164034058-1');
+    </script>
+
 </head>
 <body>
     <div id="app">
@@ -36,39 +47,6 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    {{-- <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul> --}}
                 </div>
             </div>
         </nav>
@@ -78,10 +56,4 @@
         </main>
     </div>
 </body>
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/audio.js') }}"></script> --}}
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/drag.js') }}"></script> --}}
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/audiocrop.js') }}"></script> --}}
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/effect.js') }}"></script> --}}
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/controls.js') }}"></script> --}}
-    {{-- <script type="text/javascript" src="{{ URL::asset('js/songcrop.js') }}"></script> --}}
 </html>
