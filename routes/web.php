d224c29fc8874f4b1ebcc90d916409e0c21353f4
horidhenry <robarnold100@hotmail.com>
2020-04-20 13:56:34 +0100

    unused web routes removed and marker data file added to git ignore

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/fff', function () {
-    return view('welcome');
-});
-
 Route::get('/dl', 'DashboardController@dl');
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
@@ -24,16 +20,11 @@ Route::get('/del', 'DashboardController@delete');
 Route::get('/get', 'DashboardController@getMarker');
 Route::get('/set', 'DashboardController@setMarker');
 
-Route::post('/eff', 'DashboardController@uploadEff')->name('upload-song-eff');
-Route::get('/eff', 'DashboardController@eff')->name('eff');
 
 Route::get('/loaded', 'DashboardController@loaded')->name('loaded');
 Route::post('/loaded', 'DashboardController@uploadLoaded')->name('upload-song-loaded');
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-//AJAX BOLLOCKS
-Route::post('bollocks', 'DashboardController@store');
 
 Auth::routes();
 
